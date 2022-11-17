872a965aa2f2f07d5f9edd13ff8a3c7d374dcab6
Jack <jack.harrison@atware.asia>
2022-11-17 09:06:00 +0700

    Update config

--- apps/container-management/src/environments/environment.prod.ts
+++ apps/container-management/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://192.168.0.2:3000',
-  expiredDate: '2023-05-01',
-  serialNumbers: ['9S716R512020ZL8000395', 'C02FPK6SMD6M'],
+  serverUrl: 'https://$IP:3000',
+  expiredDate: '$EXPIRED_DATE',
+  serialNumbers: ['$SERIAL_ID'],
 };
